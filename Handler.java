6ba6523ddbb78d0c8fe0b8ecebab1aceb0ad6d3f
赵巨业 <1575357879@qq.com>
2018-01-24 15:02:52 +0800

    CSTLE

---
+++ Handler.java
@@ -0,0 +1,22 @@
+package castle;
+
+public class Handler {
+	protected Game game;
+	
+    public void doCmd(String word) {
+    	
+    }
+    
+    public Handler(Game game) {
+    	this.game = game;
+    }
+    
+    public boolean isBye() {
+    	return false;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
